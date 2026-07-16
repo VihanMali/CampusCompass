72cc0e432f8e0bf0dc11f63fc47e949f431e6467
Parv Tomar <parv032007@gmail.com>
2026-07-16 11:49:49 +0530

    Chating area

--- app/discussion/page.js
+++ app/discussion/page.js
@@ -2,17 +2,21 @@
 
 import { useEffect, useState } from "react";
 import styles from "./page.module.css";
-
 export default function ChatArena() {
+  //what is this ??
   const [msg, setMsg] = useState("");
   const [username, setUsername] = useState();
   const [messages, setMessages] = useState([]);
 
+  //GetMessage that the user typed in
   async function GetMsg() {
     const response = await fetch("/api/chat");
+    //if some error happens in fetching the backend
     if (!response.ok) throw new Error(`Error ${response.status}`);
     const text = await response.text();
+    //check if text has something in it if not don't do anything
     if (!text) { setMessages([]); return; }
+    //Put the text in the messages array thingy
     setMessages(JSON.parse(text));
   }
 
@@ -21,15 +25,18 @@ export default function ChatArena() {
     const randomItem = words[Math.floor(Math.random() * words.length)];
     setUsername(randomItem + parseInt(Math.random() * (999 - 1) + 1));
 
+    //used to set an interval between any 2 messages
     GetMsg();
     const interval = setInterval(GetMsg, 3000);
     return () => clearInterval(interval);
   }, []);
 
+  //does it check if the user typed something?
   async function handleSubmit(e) {
     e.preventDefault();
     if (!msg.trim()) return;
 
+    //Gives ChatBackend the msg typed by the user and username
     await fetch("/api/chat", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -59,7 +66,7 @@ export default function ChatArena() {
               className={`${styles.messageBubble} ${isOwn ? styles.ownMessage : ""}`}
             >
               <div className={`${styles.messageAuthor} ${isOwn ? styles.ownAuthor : ""}`}>
-                {m.user}
+                 {m.username} {/* <-- changed m.user to m.username */}
               </div>
               <span className={styles.messageText}>{m.msg}</span>
             </li>
